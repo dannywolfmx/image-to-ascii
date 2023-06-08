c055893d9cae79f19dd6496f386212fb692c5f46
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2023-06-08 15:13:07 -0600

    Add cache and scale

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dannywolfmx/image-to-ascii
 
 go 1.20
+
+require golang.org/x/image v0.7.0 // indirect
--- main.go
+++ main.go
@@ -8,12 +8,20 @@ import (
 	"time"
 
 	"image/color"
+
+	"golang.org/x/image/draw"
+
 	"image/gif"
 	_ "image/png"
 )
 
 // Images no included in this repository
-const imageURL = "cow.gif"
+const imageURL = "a.gif"
+
+type GIFCache struct {
+	images string
+	delay  time.Duration
+}
 
 func main() {
 	imagefile, err := os.Open(imageURL)
@@ -27,8 +35,9 @@ func main() {
 		panic(err)
 	}
 
+	cache := generateGifCache(gifImage)
 	for {
-		printGiftImage(gifImage)
+		printGif(cache)
 	}
 }
 
@@ -49,35 +58,58 @@ func printInColor(color color.Color) string {
 	return fmt.Sprintf("\033[38;2;%d;%d;%dm\033[48;2;%d;%d;%dm█\033[0m", r, g, b, r, g, b)
 }
 
-func printImage(img image.Image) {
+func printImage(img image.Image) string {
 	var buffer strings.Builder
 	y := 0
 	countNewLine := 1
-	for ; y < img.Bounds().Max.Y; y += 3 {
+
+	//width := img.Bounds().Max.X / 2
+	//height := img.Bounds().Max.Y / 2
+
+	width := 100
+	height := 100
+
+	img = resizeImage(img, width, height)
+	for ; y < img.Bounds().Max.Y; y += 1 {
 		for x := 0; x < img.Bounds().Max.X; x += 1 {
 			buffer.WriteString(printInColor(img.At(x, y)))
-			//square character
+			buffer.WriteString(printInColor(img.At(x, y)))
+			buffer.WriteString(printInColor(img.At(x, y)))
 		}
 
 		buffer.WriteString("\n")
 		countNewLine++
 	}
-	fmt.Println(buffer.String())
 
-	//move the cursor up
-	fmt.Printf("\033[%dA", countNewLine+1)
+	buffer.WriteString(fmt.Sprintf("\033[%dA", countNewLine))
 
-	//move the cursor to the left beginning of the line
-	fmt.Printf("\033[%dD", img.Bounds().Max.X/3)
+	buffer.WriteString(fmt.Sprintf("\033[%dD", img.Bounds().Max.X))
+	return buffer.String()
 
 }
 
-func printGiftImage(img *gif.GIF) {
-	for i, frame := range img.Image {
-		printImage(frame)
-		//delay
-		fmt.Println(img.Delay[i])
+func resizeImage(src image.Image, width, height int) image.Image {
+	img := image.NewRGBA(
+		image.Rect(0, 0, width, height),
+	)
+	draw.NearestNeighbor.Scale(img, img.Bounds(), src, src.Bounds(), draw.Over, nil)
+	return img
+}
 
-		time.Sleep(time.Duration(img.Delay[i]) * time.Microsecond)
+func generateGifCache(img *gif.GIF) []GIFCache {
+	cache := make([]GIFCache, len(img.Image))
+	for i, frame := range img.Image {
+
+		cache[i].images = printImage(frame)
+		cache[i].delay = time.Duration(img.Delay[i]) * (time.Second / 100)
+	}
+
+	return cache
+}
+
+func printGif(cache []GIFCache) {
+	for _, frame := range cache {
+		fmt.Print(frame.images)
+		time.Sleep(frame.delay)
 	}
 }
